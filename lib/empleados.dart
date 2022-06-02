066e48f49fe5aa6120fb8cbf207fc4336910844e
Edwyn-Uriel-Urbina <99411844+Edwyn-Uriel-Urbina@users.noreply.github.com>
2022-06-02 03:56:47 -0600

    Update empleados.dart

--- lib/empleados.dart
+++ lib/empleados.dart
@@ -1,42 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:urbina/acerca.dart';
 import 'package:urbina/clientes.dart';
-import 'package:urbina/empleados.dart';
 import 'package:urbina/servicios.dart';
 import 'package:urbina/sesion.dart';
 import 'package:urbina/sucursales.dart';
 import 'package:urbina/desarrollador.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class EmpleadosPage extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // Application name
-      title: 'Gas Natural',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-      initialRoute: '/',
-      routes: {
-        // Cuando naveguemos hacia la ruta "/", crearemos el Widget Pagina1
-        '/paginaSesion': (context) => SesionPage(),
-        // Cuando naveguemos hacia la ruta "/second", crearemos el Widget Pagina2
-        '/paginaServicios': (context) => ServiciosPage(),
-        '/paginaEmpleados': (context) => EmpleadosPage(),
-        '/paginaClientes': (context) => ClientesPage(),
-        '/paginaAcerca': (context) => AcercaPage(),
-        '/paginaSucursales': (context) => SucursalesPage(),
-        '/paginaDesarrollador': (context) => DesarrolladorPage(),
-      },
-    );
+        debugShowCheckedModeBanner: false,
+        // Application name
+        title: 'Gas Natural',
+        // Application theme data, you can set the colors for the application as
+        // you want
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        // A widget which will be started on application startup
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
+        initialRoute: '/',
+        routes: {
+          // Cuando naveguemos hacia la ruta "/", crearemos el Widget Pagina1
+          '/paginaSesion': (context) => SesionPage(),
+          // Cuando naveguemos hacia la ruta "/second", crearemos el Widget Pagina2
+          '/paginaServicios': (context) => ServiciosPage(),
+          '/paginaEmpleados': (context) => EmpleadosPage(),
+          '/paginaClientes': (context) => ClientesPage(),
+          '/paginaAcerca': (context) => AcercaPage(),
+          '/paginaSucursales': (context) => SucursalesPage(),
+          '/paginaDesarrollador': (context) => DesarrolladorPage(),
+        });
   }
 }
 
@@ -50,7 +46,7 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         title: const Text(
-          'Inicio',
+          'Empleados',
           style: TextStyle(
             color: Colors.white,
           ),
@@ -152,60 +148,206 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
       ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color(0xFF5F73FE),
-              Color(0xCE002D62)
-            ],
-            stops: [
-              0,
-              1
-            ],
-            begin: AlignmentDirectional(1, -1),
-            end: AlignmentDirectional(-1, 1),
-          ),
-        ),
+      body: Center(
+        child: Padding(
+        padding: EdgeInsetsDirectional.fromSTEB(16, 8, 16, 0),
         child: Column(
           mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Image.network(
-              'https://raw.githubusercontent.com/Edwyn-Uriel-Urbina/Gpo-6toI-Mis-Imagenes-UII/main/Gas%20Natural/Logo.png',
-              width: 220,
-              height: 140,
-              fit: BoxFit.fitWidth,
-            ),
             Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
-              child: Text(
-                'Bienvenido/a',
-                style: TextStyle(
-                  fontFamily: 'Lexend Deca',
-                  color: Colors.white,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
+              padding: EdgeInsetsDirectional.fromSTEB(5, 5, 5, 5),
+              child: TextFormField(
+                autofocus: true,
+                obscureText: false,
+                decoration: InputDecoration(
+                  labelText: 'Nombre',
+                  hintText: 'Ingresa Nombre',
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFF95A1AC),
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFF95A1AC),
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
                 ),
+                style: TextStyle(),
               ),
             ),
             Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 120),
-              child: Text(
-                'Más económico que otros combustibles.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'Lexend Deca',
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.normal,
+              padding: EdgeInsetsDirectional.fromSTEB(5, 5, 5, 5),
+              child: TextFormField(
+                autofocus: true,
+                obscureText: false,
+                decoration: InputDecoration(
+                  labelText: 'ID',
+                  hintText: 'Ingresa ID',
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFF95A1AC),
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFF95A1AC),
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
                 ),
+                style: TextStyle(),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(5, 5, 5, 5),
+              child: TextFormField(
+                autofocus: true,
+                obscureText: false,
+                decoration: InputDecoration(
+                  labelText: 'CURP',
+                  hintText: 'Ingresa CURP',
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFF95A1AC),
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFF95A1AC),
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                style: TextStyle(),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(5, 5, 5, 5),
+              child: TextFormField(
+                autofocus: true,
+                obscureText: false,
+                decoration: InputDecoration(
+                  labelText: 'Numero de Telefono',
+                  hintText: 'Ingresa Telefono',
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFF95A1AC),
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFF95A1AC),
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                style: TextStyle(),
+              ),
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(5, 5, 5, 5),
+                    child: TextFormField(
+                      autofocus: true,
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        labelText: 'Dia de Nacimiento',
+                        hintText: 'Ingresa Dia',
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFF95A1AC),
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFF95A1AC),
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                      ),
+                      style: TextStyle(),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(5, 5, 5, 5),
+                    child: TextFormField(
+                      autofocus: true,
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        labelText: 'Mes de Nacimiento',
+                        hintText: 'Ingresa Mes',
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFF95A1AC),
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFF95A1AC),
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                      ),
+                      style: TextStyle(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(5, 5, 5, 5),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                  side: BorderSide(color: Color.fromRGBO(95, 115, 254, 1), width: 1),
+                  primary: Color.fromRGBO(95, 115, 254, 1),
+                  onPrimary: Colors.white,
+                ),
+                child: const Text('Guardar'),
+                onPressed: () {},
               ),
             ),
           ],
         ),
+      )
       ),
     );
   }
